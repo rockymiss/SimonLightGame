34936a6e73c6f6ca748253374fe26b6707c6e12b
rockymiss <rachelmcdermottrock@gmail.com>
2022-06-08 21:03:55 +0000

    update functions and tests

---
+++ script/game.js
@@ -0,0 +1,28 @@
+let game = {
+    score: 0,
+    currentGame: [],
+    playerMoves: [],
+    choices: ["button1", "button2", "button3", "button4"],
+}
+
+
+function newGame() {
+    game.score = 0;
+    game.playerMoves = [];
+    game.currentGame = [];
+    showScore();
+    addTurn();
+}
+
+function addTurn() { 
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    //showTurns();
+}
+
+function showScore() {
+    document.getElementById("score").innerText = game.score;
+}
+
+
+module.exports = { game, newGame, showScore, addTurn };
